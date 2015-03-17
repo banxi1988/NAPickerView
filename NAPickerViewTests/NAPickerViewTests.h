f531a60c0b7e483cfc2007e680208dadaf13c275
banxi1988 <banxi1988@gmail.com>
2015-03-17 09:25:55 +0800

    convert to XCTest Case

--- NAPickerViewTests/NAPickerViewTests.h
+++ NAPickerViewTests/NAPickerViewTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 nghialv. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface NAPickerViewTests : SenTestCase
+@interface NAPickerViewTests : XCTestCase
 
 @end
